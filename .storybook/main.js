9895c92659b8b795fa22d696b22b0ec4864b7d16
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-01-24 21:49:07 +1100

    older post css

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,27 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/stories/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-notes/register',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  webpackFinal: async config => {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: require.resolve("babel-loader"),
+            options: {
+              presets: [require.resolve("babel-preset-react-app")]
+            }
+          },
+          require.resolve("react-docgen-typescript-loader")
+        ]
+      }
+    ],
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './scss/tailwind.scss';
 
 function App() {
   return (
